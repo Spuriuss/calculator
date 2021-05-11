6f874ea436b6c6fb6b4b5650f4d1185b76cce379
spurius <spurius.3981@gmail.com>
2021-05-11 13:25:59 +0300

    sdas

--- code.cpp
+++ code.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 
-int skadoosh(int, char[], int[], int[], int[], int, int, int[], int);
+int skadoosh(int, char[], int[], int[], int[], int, int, int[], int, int);
 int justPlusingMaaaaaaaaan(char[], int[], int[], int[], int, int[], int, int);
 int brackets(char[], int[], int[], int[], int[], int, int, int, int);
 
@@ -18,17 +18,26 @@ int brackets(char[], int[], int[], int[], int[], int, int, int, int);
 
 
 
-int skadoosh(int startingPoint, char charArray[], int numberArray[], int firstOrderOperators[], int secondOrderOperators[], int operatorsCounter, int skobkiPointsCounter, int skobkiPoints[], int NumberOfTheNumber0xD) {
+int skadoosh(int startingPoint, char charArray[], int numberArray[], int firstOrderOperators[], int secondOrderOperators[], int operatorsCounter, int skobkiPointsCounter, int skobkiPoints[], int NumberOfTheNumber0xD, int fromBracketsStartingNumber) {
 	cout << "skadoosh!" << endl;
 	
-	int result = numberArray[startingPoint];
-	int clusterCounter = 0; // how many operations have there been so far
 	
+
+	
+	int result;
+	if (fromBracketsStartingNumber != 999999) { // если отдаём skadoosh какое-то "первое" число, являющееся результатом скобок, используем его. Если нет - используем starting point
+		result = fromBracketsStartingNumber;
+	} else {
+		result = numberArray[startingPoint];
+	}
+	cout << "I am skadooch, I picked up the result, and it is: " << result << endl;
+	int clusterCounter = 0; // how many operations have there been so far
 	int firstSkobka;
 	int bracketsValue;
+	cout << "skadoosh startingPoint: " << startingPoint << endl;
 	for (int i = startingPoint; firstOrderOperators[i] != 999999 && startingPoint + clusterCounter != operatorsCounter; i++) {
 		firstSkobka = 999999;
-		
+		cout << 333333333333 << endl;
 		
 		for (int d = 0; d < skobkiPointsCounter; d++) { // brackets 
 			if (skobkiPoints[d] == i) {
@@ -37,6 +46,7 @@ int skadoosh(int startingPoint, char charArray[], int numberArray[], int firstOr
 		}
 
 		if (firstSkobka != 999999) {
+			//cout << 2222222222222222;
 			bracketsValue = brackets(charArray, numberArray, firstOrderOperators, secondOrderOperators, skobkiPoints, firstSkobka, operatorsCounter, skobkiPointsCounter, NumberOfTheNumber0xD); //SUMMON!!1
 			
 			if (charArray[firstOrderOperators[i]] == '*') {
@@ -47,16 +57,18 @@ int skadoosh(int startingPoint, char charArray[], int numberArray[], int firstOr
 			}
 			clusterCounter++;
 		} else  {
-		if (charArray[firstOrderOperators[i]] == '*') {
-			result = result * numberArray[i+1];
+			//cout << 111111111111111111;
+			if (charArray[firstOrderOperators[i]] == '*') { // regular
+				result = result * numberArray[i+1];
+			}
+			if (charArray[firstOrderOperators[i]] == '/') {
+				result = result / numberArray[i+1];
+			}
+			clusterCounter++;
 		}
-		if (charArray[firstOrderOperators[i]] == '/') {
-			result = result / numberArray[i+1];
-		}
-		clusterCounter++;
+		firstOrderOperators[i] = 999999;
 	}
-	}
-cout << "result: " << result << endl;
+cout << "skadoosh result: " << result << endl;
 return result;
 
 }
@@ -80,18 +92,17 @@ return result;
 
 
 
+
+
+
+
 int justPlusingMaaaaaaaaan(char charArray[], int numberArray[], int firstOrderOperators[], int secondOrderOperators[], int operatorsCounter, int skobkiPoints[], int skobkiPointsCounter, int NumberOfTheNumber0xD) {
 	cout << "justPlusingMaaaaaaaaan" << endl;
-	cout << "just plusing firstOrderOperators: " << "\n";
-for (int i = 0; i < operatorsCounter; i++) {
-	cout << firstOrderOperators[i] << "\n";
-}
-	cout << "just plusing secondOrderOperators: " << "\n";
-for (int i = 0; i < operatorsCounter; i++) {
-	cout << secondOrderOperators[i] << "\n";
-}
+
+
 	int startingPoint; // for skadoosh
-	//int result = numberArray[0];
+	int firstSkobka;
+	int fromBracketsStartingNumber = 999999; // just plusing никогда не отдаёт skadoosh "первое число", только "starting Point". "Первое число" - только для brackets
 	int justPlusingFirstNumber;
 	for (int i = 0; i < operatorsCounter; i++) { // определяем первое число для result (при рекурсии оно не будет просто первым числом числового массива)
 		if (secondOrderOperators[i] != 999999) {
@@ -100,6 +111,29 @@ for (int i = 0; i < operatorsCounter; i++) {
 		}
 	}
 	int result = numberArray[justPlusingFirstNumber];
+
+	
+	
+	
+
+		
+
+		for (int i = 0; i < skobkiPointsCounter; i++) { // edge case если скобки идут сначала
+		
+		cout << 1111 << endl;
+			if (skobkiPoints[i] != 999999) {
+				cout << 2222 << endl;
+				//cout << "firstOrderOperators[skobkiPoints[" << i << "]]: " << firstOrderOperators[skobkiPoints[i]] << endl;
+				//cout << "secondOrderOperators[skobkiPoints[" << i << "]]: " << secondOrderOperators[skobkiPoints[i]] << endl;
+				if ((firstOrderOperators[skobkiPoints[i]] == 999999 && secondOrderOperators[skobkiPoints[i]] == 999999) || skobkiPoints[i] == -1) {
+					cout << "brackets edge case" << endl;
+					firstSkobka = i;
+					result = brackets(charArray, numberArray, firstOrderOperators, secondOrderOperators, skobkiPoints, firstSkobka, operatorsCounter, skobkiPointsCounter, NumberOfTheNumber0xD);
+				}
+			}
+		}
+	
+
 	
 	int firstFirstOrderOperator = 999999; // edge case если сначала стоит кластер умножения (при рекурсии оно не будет просто "если первое умножение не пустое", при рекурсии это "если первое умножение стоит раньше первого сложения")
 	int firstSecondOrderOperator = 999999;
@@ -116,21 +150,20 @@ for (int i = 0; i < operatorsCounter; i++) {
 		}
 	}
 	if (firstFirstOrderOperator < firstSecondOrderOperator) {
-		cout << "edge case" << endl;
+		cout << "skadoosh edge case" << endl;
 		startingPoint = firstFirstOrderOperator;
-		result = skadoosh(startingPoint, charArray, numberArray, firstOrderOperators, secondOrderOperators, operatorsCounter, skobkiPointsCounter, skobkiPoints, NumberOfTheNumber0xD);
+		result = skadoosh(startingPoint, charArray, numberArray, firstOrderOperators, secondOrderOperators, operatorsCounter, skobkiPointsCounter, skobkiPoints, NumberOfTheNumber0xD, fromBracketsStartingNumber);
 	}
 	
 	
-
+	
 	
 	int skadooshValue;
 	int bracketsValue;
 	
-	int firstSkobka;
+	
 	for (int i = 0; i < operatorsCounter; i++) {
 		cout << "i: " << i << endl;
-		cout << "secondOrderOperators: " << secondOrderOperators[i] << endl;
 		firstSkobka = 999999; // "очищаем" оператор "есть ли тут скобка"
 		
 		for (int d = 0; d < skobkiPointsCounter; d++) { // brackets 
@@ -148,16 +181,11 @@ for (int i = 0; i < operatorsCounter; i++) {
 					result = result - bracketsValue;
 				}
 		} else
-			
-			
 
-		
-		
-		
-		
+
 		if (secondOrderOperators[i] != 999999 && firstOrderOperators[i+1] != 999999 && i != (operatorsCounter - 1)) { // SKADOOSH!!
 			startingPoint = i + 1;
-			skadooshValue = skadoosh(startingPoint, charArray, numberArray, firstOrderOperators, secondOrderOperators, operatorsCounter, skobkiPointsCounter, skobkiPoints, NumberOfTheNumber0xD);
+			skadooshValue = skadoosh(startingPoint, charArray, numberArray, firstOrderOperators, secondOrderOperators, operatorsCounter, skobkiPointsCounter, skobkiPoints, NumberOfTheNumber0xD, fromBracketsStartingNumber);
 			// SUMMON!!1
 			if (charArray[secondOrderOperators[i]] == '+') { // regular after skadoosh
 				result = result + skadooshValue;
@@ -167,6 +195,7 @@ for (int i = 0; i < operatorsCounter; i++) {
 			}
 		} else
 		if (secondOrderOperators[i] != 999999) { // regular
+	cout << 11 << endl;
 			if (charArray[secondOrderOperators[i]] == '+') { 
 				
 				result = result + numberArray[i+1];
@@ -176,9 +205,29 @@ for (int i = 0; i < operatorsCounter; i++) {
 			}
 		}
 	}
-	cout << "done: " << result << endl;
+	cout << "just plusing done: " << result << endl;
 	return result;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -189,68 +238,126 @@ int brackets(char charArray[], int numberArray[], int firstOrderOperators[], int
 	int result;
 	
 	
-	int secondSkobka = skobkiPointsCounter-1-firstSkobka;
-	int subOperatorCounter = skobkiPoints[secondSkobka]-skobkiPoints[firstSkobka];
-	int subNumberCounter = subOperatorCounter + 1;
 	
-		cout << "firstSkobka: " << firstSkobka << endl;
-		cout << "secondSkobka: " << secondSkobka << endl;
-		cout << "operatorsCounter: " << operatorsCounter << endl;
-		cout << "sub arrays work in progress..." << endl;
-	int *subNumbers = new int[subNumberCounter];
+	cout << "firstSkobka: " << firstSkobka << endl;
+	
+	int secondSkobka;
+	int skip = 0;
+	for (int i = firstSkobka + 1; i < skobkiPointsCounter; i++) {
+		cout << "second skobka i: " << i << endl;
+
+		if (skobkiPoints[i] == operatorsCounter) {
+			secondSkobka = i;
+			break;
+		}
+		if (secondOrderOperators[skobkiPoints[i]] != 999999) {
+			
+			if (charArray[secondOrderOperators[skobkiPoints[i]]-1] == ')') {
+				if (skip != 0) {
+					skip--;
+				} else {
+					secondSkobka = i;
+					break;
+				}
+			}
+			if (charArray[secondOrderOperators[skobkiPoints[i]]+1] == '(') {
+				skip++;
+			}
+		}
+		if (firstOrderOperators[skobkiPoints[i]] != 999999) {
+			
+			if (charArray[firstOrderOperators[skobkiPoints[i]]-1] == ')') {
+				if (skip != 0) {
+					skip--;
+				} else {
+					secondSkobka = i;
+					break;
+				}
+			}
+			if (charArray[firstOrderOperators[skobkiPoints[i]]+1] == '(') {
+				skip++;
+			}
+		}
+	
+	}
+		
+		
+
+		
+	
+	cout << "secondSkobka: " << secondSkobka << endl;	
+	
+	
+	
+	int subOperatorCounter = skobkiPoints[secondSkobka]-skobkiPoints[firstSkobka] - 1;
+
+	
+		
+
+
 	int *subFirstOrderOperators = new int[operatorsCounter];
 	int *subSecondOrderOperators = new int[operatorsCounter];
+	int *subSkobkiPoints = new int[skobkiPointsCounter];
 	
-
+	for (int i = 0; i < skobkiPointsCounter; i++) {
+		if (i > firstSkobka && i < secondSkobka) {
+			subSkobkiPoints[i] = skobkiPoints[i];
+		} else {
+			subSkobkiPoints[i] = 999999;
+		}
+	}
 	
-
 	
 	
 	for (int g = 0; g < operatorsCounter; g++) { // подмассив "скобочных" операторов первого порядка
 		
 		
 		
-		if (g > skobkiPoints[firstSkobka] && g < (skobkiPoints[secondSkobka] + 1)) {
+		if (g > skobkiPoints[firstSkobka] && g < (skobkiPoints[secondSkobka])) {
 			subFirstOrderOperators[g] = firstOrderOperators[g];
 			firstOrderOperators[g] = 999999;
 		} else {
 			subFirstOrderOperators[g] = 999999;
 		}
-	
-	
-
 	}
-	
-	//cout << "subSecondOrderOperators in progress..." << endl;
 	for (int g = 0; g < operatorsCounter; g++) { // подмассив "скобочных" операторов второго порядка
-
-		
-		if (g > skobkiPoints[firstSkobka] && g < (skobkiPoints[skobkiPointsCounter-1-firstSkobka]) + 1) {
+	
+	
+		if (g > skobkiPoints[firstSkobka] && g < (skobkiPoints[secondSkobka])) {
 			subSecondOrderOperators[g] = secondOrderOperators[g];
 			secondOrderOperators[g] = 999999;
 		} else {
 			subSecondOrderOperators[g] = 999999;
 		}
-		
 	}
 	
-	
-	skobkiPoints[firstSkobka] = 999999;
+	int secondSkobkaOperator = skobkiPoints[secondSkobka];
+	skobkiPoints[firstSkobka] = 999999; 
 	skobkiPoints[secondSkobka] = 999999;
+
+	// we have to clear it before sending it down to just plusing, but we need to save the last operator to check if the one after it is multiplication, for skadoosh afetr plusing
+
+	
+	result = justPlusingMaaaaaaaaan(charArray, numberArray, subFirstOrderOperators, subSecondOrderOperators, operatorsCounter, subSkobkiPoints, skobkiPointsCounter, NumberOfTheNumber0xD); // SUMMON!!1
+	
 	
 	
 
-cout << "subFirstOrderOperators: " << endl;
-for (int i = 0; i < operatorsCounter; i++) {
-	cout << subFirstOrderOperators[i] << endl;
-}
-cout << "subSecondOrderOperators: " << endl;
-for (int i = 0; i < operatorsCounter; i++) {
-	cout << subSecondOrderOperators[i] << endl;
-}
+
 	
-	result = justPlusingMaaaaaaaaan(charArray, numberArray, subFirstOrderOperators, subSecondOrderOperators, operatorsCounter, skobkiPoints, skobkiPointsCounter, NumberOfTheNumber0xD); // SUMMON!!1
-	delete subNumbers;
+	
+
+	if (firstOrderOperators[secondSkobkaOperator] != 999999 && secondSkobkaOperator != operatorsCounter) {
+	
+		cout << "skadoosh from brackets" << endl;
+		int fromBracketsStartingNumber = result;
+		cout << "fromBracketsStartingNumber: " << fromBracketsStartingNumber << endl;
+		int startingPoint = secondSkobkaOperator;
+		result = skadoosh(startingPoint, charArray, numberArray, firstOrderOperators, secondOrderOperators, operatorsCounter, skobkiPointsCounter, skobkiPoints, NumberOfTheNumber0xD, fromBracketsStartingNumber);
+	}
+	
+
+
 	delete subFirstOrderOperators;
 	delete subSecondOrderOperators;
 	
@@ -305,8 +412,12 @@ for (int i = 0; i < charArraySize; i++) {
 			}
 	} else 
 	if (charArray[i] == '(' || charArray[i] == ')') {
-		if (charArray[i] == '(') {skobkiPoints[skobkiPointsCounter] = (breakPointsCounter - 1);} // скобки отсылаются не на номера скобок в символьном массиве, а на порядковые номера операторов. Предыдущего - в случае, если скобка открывающая, и следующего - если закрывающая. Если счётчик имеет значение ноль - это не значит, что предыдущий оператор был "нулевой". Это значит, что никакого оператора ещё вообще не было. Hense "-1". А если счётчик равен 1, то последний оператор был нулевой, а следующий будет первый.
-		if (charArray[i] == ')') {skobkiPoints[skobkiPointsCounter] = (breakPointsCounter-1);}
+		if (charArray[i] == '(') {
+			skobkiPoints[skobkiPointsCounter] = (breakPointsCounter - 1);
+		}
+		if (charArray[i] == ')') {
+				skobkiPoints[skobkiPointsCounter] = (breakPointsCounter);
+		}
 		skobkiPointsCounter++;
 	} else {
 		numberPoints[numberPointsCounter] = i;
@@ -372,8 +483,8 @@ cout << "--------------------------------------" << "\n";
 // operators sorting system
 
 
-int *firstOrderOperators = new int[breakPointsCounter];
-int *secondOrderOperators = new int[breakPointsCounter];
+int *firstOrderOperators = new int[breakPointsCounter+1];
+int *secondOrderOperators = new int[breakPointsCounter+1];
 int operatorsCounter = 0;
 for (int i = 0; i < breakPointsCounter; i++) {
 	if (charArray[breakPoints[i]] == '*' || charArray[breakPoints[i]] == '/') {
